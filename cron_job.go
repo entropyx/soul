625d904067a96346b74c86bc624d154944ff5a59
Daniel Montoya <dsmontoyam@gmail.com>
2018-10-19 12:55:34 -0500

    cronjob command

---
+++ cron_job.go
@@ -0,0 +1,22 @@
+package soul
+
+import (
+	"github.com/robfig/cron"
+	log "github.com/sirupsen/logrus"
+)
+
+type cronJob struct {
+	name    string
+	spec    string
+	handler func()
+}
+
+func (c *cronJob) Start() {
+	log.Info("Starting cronjob")
+	cj := cron.New()
+	cj.AddFunc(c.spec, func() {
+		log.Info("Running cronjob")
+		c.handler()
+	})
+	cj.Start()
+}
