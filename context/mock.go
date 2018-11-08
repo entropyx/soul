6236b27fde1869973a845e81a97dbd253e7e83db
Daniel Montoya <dsmontoyam@gmail.com>
2018-11-08 14:46:23 -0600

    Response is passed through the struct

--- context/mock.go
+++ context/mock.go
@@ -1,10 +1,11 @@
 package context
 
 type MockContext struct {
+	Response *R
 }
 
-func (m *MockContext) Publish(*R) {
-
+func (m *MockContext) Publish(r *R) {
+	m.Response = r
 }
 func (m *MockContext) Request() *R {
 	return &R{Headers: M{}}
