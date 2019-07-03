18b730c1470fb0ce8569ab19cd871c6e3a09dded
Daniel Montoya <dsmontoyam@gmail.com>
2019-07-03 17:11:13 -0500

    Set sampler depending on the env

--- middlewares/opencensus.go
+++ middlewares/opencensus.go
@@ -3,6 +3,7 @@ package middlewares
 import (
 	"github.com/entropyx/opencensus-propagation"
 	"github.com/entropyx/soul/context"
+	"github.com/entropyx/soul/env"
 	"github.com/entropyx/soul/tracers"
 	"go.opencensus.io/trace"
 )
@@ -19,7 +20,7 @@ func OpenCensus() context.Handler {
 
 		spanCtx, _ := propagation.Extract(propagation.FormatTextMap, c.Request.Headers)
 
-		_, span := trace.StartSpanWithRemoteParent(c, "new-request", spanCtx, trace.WithSpanKind(trace.SpanKindServer))
+		_, span := trace.StartSpanWithRemoteParent(c, "new-request", spanCtx, trace.WithSpanKind(trace.SpanKindServer), trace.WithSampler(setSampler()))
 		defer span.End()
 
 		propagation.Inject(span.SpanContext(), propagation.FormatTextMap, c.Headers)
@@ -33,5 +34,16 @@ func OpenCensus() context.Handler {
 		if err := c.Error; err != nil {
 			tracer.SetErrorTag(span, err)
 		}
+	}
+}
+
+func setSampler() trace.Sampler {
+	switch env.Mode {
+	case env.ModeProduction, env.ModeStaging:
+		return trace.AlwaysSample()
+	case env.ModeTest, env.ModeDebug:
+		return trace.NeverSample()
+	default:
+		return trace.AlwaysSample()
 	}
 }
