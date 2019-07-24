e1d2ca5be0432d9078c9fb4e91e11d7593bcc22e
Daniel Montoya <dsmontoyam@gmail.com>
2019-07-24 16:36:17 -0500

    tracers: datadog logs from stackdriver

---
+++ tracers/datadog_stackdriver.go
@@ -0,0 +1,49 @@
+package tracers
+
+import (
+	"encoding/binary"
+	"encoding/hex"
+
+	propagation "github.com/entropyx/opencensus-propagation"
+	"github.com/entropyx/soul/context"
+	"github.com/entropyx/soul/log"
+	opentracing "github.com/opentracing/opentracing-go"
+	"go.opencensus.io/trace"
+)
+
+type DatadogSd struct{}
+
+var _ Tracer = &DatadogSd{}
+
+func (*DatadogSd) LogFields(m context.M, logger log.Logger) log.Logger {
+	var traceID [16]byte
+	var spanID [8]byte
+	decodeAndCopyString(m[propagation.HeaderTraceID].(string), traceID[:])
+	decodeAndCopyString(m[propagation.HeaderSpanID].(string), spanID[:])
+	uTraceID := binary.BigEndian.Uint64(traceID[8:])
+	uSpanID := binary.BigEndian.Uint64(spanID[:])
+	newLogger := logger.WithField("trace_id", uTraceID)
+	newLogger = newLogger.WithField("span_id", uSpanID)
+	return newLogger
+}
+
+func (*DatadogSd) SetErrorTag(span interface{}, err error) {
+	switch s := span.(type) {
+	case opentracing.Span:
+		// TODO: OpenTracing
+	case trace.Span:
+		s.SetStatus(trace.Status{
+			Code:    trace.StatusCodeUnknown, // TODO: code strategy
+			Message: err.Error(),
+		})
+	}
+}
+
+func decodeAndCopyString(s string, dst []byte) error {
+	buf, err := hex.DecodeString(s)
+	if err != nil {
+		return err
+	}
+	copy(dst, buf)
+	return nil
+}
